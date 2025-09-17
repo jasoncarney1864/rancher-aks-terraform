8983381c99fc4af1856d6c2a14d26fe61becfabc
jasoncarney1864 <jasoncarney1864@gmail.com>
2025-09-17 18:30:05 -0400

    Another adjustment to provider Helm

--- providers.tf
+++ providers.tf
@@ -1,20 +1,24 @@
+# Azure provider
 provider "azurerm" {
   features {}
 }
 
-# Authenticate to Azure via Azure CLI (recommended) or env vars
-# - Azure CLI: az login ; az account set --subscription "<SUBSCRIPTION_ID_OR_NAME>"
-# - Env vars: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID (for service principal)
-
-# These providers will connect to AKS once it's created
-# Configure Kubernetes provider
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+# Fetch kubeconfig from AKS
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = azurerm_kubernetes_cluster.aks.name
+  resource_group_name = azurerm_kubernetes_cluster.aks.resource_group_name
+  depends_on          = [azurerm_kubernetes_cluster.aks]
 }
 
+# Kubernetes provider
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
+# Helm provider (no kubernetes block anymore)
 provider "helm" {
-  # Helm will automatically reuse the default kubernetes provider
+  # Helm will automatically use the default kubernetes provider
 }
