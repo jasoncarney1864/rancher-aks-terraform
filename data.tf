be6c1ef3d6ca54c82d4e7eacd09b6a6bee4f37a6
jasoncarney1864 <jasoncarney1864@gmail.com>
2025-09-17 19:04:32 -0400

    Added data.tf

---
+++ data.tf
@@ -0,0 +1,8 @@
+data "kubernetes_service" "ingress_nginx_controller" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = "ingress-nginx"
+  }
+
+  depends_on = [helm_release.ingress_nginx]
+}
